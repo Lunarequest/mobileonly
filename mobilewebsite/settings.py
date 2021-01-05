cdbd928b31ce68d17410925642a03116c7822246
advaith madhukar <advaith.madhukar@gmail.com>
2021-01-05 13:29:41 +0000

    set debug to false

--- mobilewebsite/settings.py
+++ mobilewebsite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=os.environ.get("key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',"mobiledevwebsite.herokuapp.com"]
 
